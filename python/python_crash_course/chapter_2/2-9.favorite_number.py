84e35efab3b2bf3f24f2813d2a48effd4c6262ec
James Noria <jamesnoria@gmail.com>
2020-07-21 09:27:47 -0500

    chapter 2 done

---
+++ python/python_crash_course/chapter_2/2-9.favorite_number.py
@@ -0,0 +1,6 @@
+""" Store your favorite number in a variable. Then, using that
+variable, create a message that reveals your favorite number.
+Print that message. """
+
+favorite_number = 10
+print('My favorite number is:\n', favorite_number)
