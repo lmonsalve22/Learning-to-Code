e80e004f244ff16df15000ee2fbc7b2fc4ecb286
James Noria <jamesnoria@gmail.com>
2020-07-23 00:54:40 -0500

    chapter 6 loading

---
+++ python/python_crash_course/chapter_6/6-4.glossary_2.py
@@ -0,0 +1,4 @@
+""" it's the same exercise of 6-3 just adding
+5 new elements. """
+
+print('I will not waste my time with this')
---
+++ python/python_crash_course/chapter_6/6-9.favorite_places.py
@@ -0,0 +1,9 @@
+
+
+favorite_places = {
+    'james' : ['brazil', 'cuzco', 'campinas'],
+    'cynthya' : ['argentina', 'san marcos'],
+    'carolina' : ['peru', 'san francisco']
+}
+
+
