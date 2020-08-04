9e5f567532ef2cd9c4f7dd4403e0bf9686920c67
James Noria <jamesnoria@gmail.com>
2020-08-03 22:52:57 -0500

    project 1.3

--- python/python_crash_course/project_alien_invasion/alien_invasion.py
+++ python/python_crash_course/project_alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -13,9 +14,12 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
 
+    bullets = Group()
+
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
---
+++ python/python_crash_course/project_alien_invasion/rocket_settings.py
@@ -0,0 +1,8 @@
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1366
+        self.screen_height = 700
+        self.bg_color = (128, 128, 0)
+
+        self.rocket_speed_factor = 1.5
