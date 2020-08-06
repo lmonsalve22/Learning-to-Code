1253fc773891cee9ad2469dc0c567888a2069c9c
James Noria <jamesnoria@gmail.com>
2020-08-05 23:00:50 -0500

    project finished

--- python/python_crash_course/project_alien_invasion/ship.py
+++ python/python_crash_course/project_alien_invasion/ship.py
@@ -1,14 +1,17 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
+        super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
+     
         self.rect.bottom = self.screen_rect.bottom
 
         self.center = float(self.rect.centerx)
@@ -26,3 +29,6 @@ class Ship:
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
